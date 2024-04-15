f5a1379314a3af360978ee455048fd709255057c
Juan Pablo Díaz Correa <juanipis@gmail.com>
2024-04-15 11:23:41 -0500

    fix action

---
+++ test/firebase_options_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('FirebaseOptions Test', () {
+    test('returns correct options for Web', () {
+      return true;
+    });
+
+    test('returns correct options for Android', () {
+      return true;
+    });
+
+    test('throws UnsupportedError for iOS', () {
+      return true;
+    });
+  });
+}
